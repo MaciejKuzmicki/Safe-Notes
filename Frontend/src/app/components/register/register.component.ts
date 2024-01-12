745baf6356cba3d9673cd020fafde48fd14409c8
macius02 <mkuzmicki2002@gmail.com>
2024-01-12 14:22:44 +0100

    Started auth on frontend

--- Frontend/src/app/components/register/register.component.ts
+++ Frontend/src/app/components/register/register.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+  myForm!: FormGroup;
+  private formBuilder: any;
+
+  constructor(private formbuilder: FormBuilder) {
+
+  }
+
+  ngOnInit(): void {
+    this.myForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.minLength(1), Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      password2: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
 
 }
